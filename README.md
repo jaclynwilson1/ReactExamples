d166bf72951bbffcd04b5c14b89470ef5d814880
Jaclyn Wilson <jaclyn@Jaclyns-MacBook-Pro.local>
2023-04-11 17:13:37 -0400

    Tetris starter code

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ npx create-react-app react-app-one
 cd react-app-one
 npm start
 ```
+
+## react-app-one
+this is my hello world app for understanding Reactr
+
+## tetris
+This is a mimic easy version of tetris
---
+++ tetris/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <div class="jumbotron">
+      <h1>Welcome to Tetris by JCW</h1>
+      </div>
+      <h1>Include Box here</h1>
+
+    </div>
+  );
+}
+
+export default App;
